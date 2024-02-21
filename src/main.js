eb0c9d5f757349ac8d5cde1ec8ec84a3fc665c47
mooneryy <yelisyeyevaa@gmail.com>
2024-02-21 15:06:46 +0200

    html

---
+++ src/main.js
@@ -0,0 +1,14 @@
+//бібліотеки
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+const KEY = '42472601-e2efb745d6431960b7108569a';
+const SOURCE_URL = 'https://pixabay.com/api/';
+const QUERY = 'stray+cats';
+const LINK = `${SOURCE_URL}?key=${KEY}&q=${QUERY}`;
+
+
